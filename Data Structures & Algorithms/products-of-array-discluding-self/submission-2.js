756af349edcdd2979fda857e4ee2c3bd46af6993
HarshithGowdaM <139833833+Harshith07-dot@users.noreply.github.com>
2026-08-07 22:48:03 +0530

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let res = [];
+        let n = nums.length;
+        let left = new Array(n);
+        let right = new Array(n);
+        left[0] = 1;
+        right[n-1] = 1;
+        for(let i=1;i<n;i++){
+            left[i] = nums[i-1] * left[i-1];
+        }
+        for(let i = n-2;i>=0;i--){
+            right[i] = nums[i+1] * right[i+1];
+        }
+        for(let i=0;i<n;i++){
+            res[i] = left[i] * right[i];
+        }
+        return res;
+    }
+}
