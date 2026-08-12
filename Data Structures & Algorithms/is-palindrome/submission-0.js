dc10bf1f6be02ca9ef023ee94f4516094b0907f1
HarshithGowdaM <139833833+Harshith07-dot@users.noreply.github.com>
2026-08-12 22:52:01 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        const s1 = s.replace(/[^a-zA-Z0-9]/g,'').toLowerCase();
+        let i = 0;
+        let j=s1.length-1;
+        while(i<j){
+            if(s1[i] === s1[j]){
+                i++;
+                j--
+            } else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
