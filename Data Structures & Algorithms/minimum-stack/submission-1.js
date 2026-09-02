e79cd36f976c545f45b5f26db7c5dc8733f9759f
HarshithGowdaM <139833833+Harshith07-dot@users.noreply.github.com>
2026-09-02 17:36:03 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.js
@@ -0,0 +1,39 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+         this.minstack = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val);
+        val = Math.min(val, this.minstack.length > 0?
+        this.minstack[this.minstack.length-1] : val);
+        this.minstack.push(val);
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        this.minstack.pop();
+        this.stack.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length-1];
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.minstack[this.minstack.length-1];
+    }
+}
