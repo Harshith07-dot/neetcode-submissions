24c3f0fb981e29cbe9fc318722e265119a9e84df
HarshithGowdaM <139833833+Harshith07-dot@users.noreply.github.com>
2026-09-04 00:42:59 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        let n=temperatures.length;
+        let res = new Array(n).fill(0);
+        for(let i=n-2;i>=0;i--){
+            let j=i+1;
+            while(j<n && temperatures[j] <= temperatures[i]){
+                if(res[j] === 0){
+                    j=n;
+                    break;
+                }
+                j += res[j];
+            }
+            if(j<n){
+                res[i] = j-i;
+            }
+        }
+        return res;
+    }
+}
